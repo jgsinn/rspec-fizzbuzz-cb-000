c6ffba2879f8fd2455b3c6f837c4c13db9a3e6db
Justin Sinn <justin.sinn.co@gmail.com>
2018-07-31 00:01:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if number % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if number % 5 == 0 && number % 3 == 0 # if the number int is divisible by 3
+    "FizzBuzz" # Go fizz
+  elseif number % 3 == 0
+    "Fizz"
   elseif number % 5 == 0
     "Buzz"
-  elseif number % 5 == 0 && number % 3 == 0
-    "FizzBuzz"
   else
     nil
   end
